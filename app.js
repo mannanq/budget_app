a3d185b2b9fcf1ce1584247af03f2949b174bde6
Mannan Qazi <mannanqazi2@gmail.com>
2019-02-25 17:08:59 -0500

    Added detailed comments on defining data structs and logic

--- app.js
+++ app.js
@@ -26,21 +26,45 @@ var budgetController = (function() {
     }
   };
 
+  // budget controller
+
   return {
     addItem: function(type, des, val) {
       var newItem, ID;
 
+      // Giving an item an id:
+
+      /* Initially, if there's no item in either array of the "allItems" property, it will throw an error because there's no item yet and hence no id! */
+
+      // Check if there's an item already in the array:
+
       if (data.allItems[type].length > 0) {
+        /* We want an easy ID, like 0, 1, 2, 3... and so on.
+
+       Say we already have 2 items in "exp" with IDs 0 and 1 resp. Third item we add should have ID of 2
+       data.allItems['exp'][data.allItems['exp'].length - 1].id = 1 i.e. ID of the second item in the "exp" array(We're using ".id" bc the newItem created is an Object based on the constructor function!). Just add 1 to it and gives you 2. This will ensure the new item will always have an ID of the last item + 1     
+       */
+
         ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
       } else {
+        // If there's no item, then ID of the first item added is "0"
+
         ID = 0;
       }
 
+      // Check the "type" of the item being added
+
       if (type === 'exp') {
+        // if the type is "exp", create an object of the type "Expense" (defined above in lines 5-9) called "newItem".
+
         newItem = new Expense(ID, des, val);
       } else if (type === 'inc') {
+        // if the type is "inc", create an object of the type "Income" (defined above in lines 11-15) called "newItem".
+
         newItem = new Income(ID, des, val);
       }
+
+      // After checking the "type", add the item to the respective array of the "allItems" property in the data object
 
       data.allItems[type].push(newItem);
     },
@@ -93,7 +117,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     // "enter" key event listener if "enter" key is pressed by user while adding a budget item
 
     document.addEventListener('keypress', function(e) {
-      // Only listen if "return/enter" key is pressed ("keyCode" item 13 for new broswers. For old browsers, "which" item in the event Object is 13)
+      /* Only listen if "return/enter" key is pressed ("keyCode" item 13 for new broswers. For old browsers, "which" item in the event Object is 13) */
+
       if (e.keyCode === 13 || e.which === 13) {
         ctrlAddItem();
       }
@@ -105,10 +130,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   var ctrlAddItem = function() {
     var input, newItem;
 
-    // 1. Get field input values
+    /* 1. Get field input values from UI control by invoking ".getValues()" method. Remember, that method returns the "type", "description" and "value" of the item entered. (See lines 71-73 from the code above) */
+
     input = UICtrl.getValues();
 
-    // 2. Add new item to the budget controller
+    /* 2. Add this input i.e. new item we got from the UI controller (line 112) to the budget controller module by invoking the ".addItem()" method from "budgetController" module. Remember, that method takes "type", "description" and "value" as arguments! (See line 32 of the code above) */
+
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add new item to the UI
