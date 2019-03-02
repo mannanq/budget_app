e31dc0bf2e3a34a6402e25da0e27522c88249a05
Mannan Qazi <mannanqazi2@gmail.com>
2019-03-02 18:59:22 -0500

    Added: method for cleaning fields, refactored global controller, method for budget calcs, and deailed code comments

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  // this function is called by the "calculateBudget" function (public method -- see line 89)
+  var calculateTotal = function(type) {
+    /* the method gets the type as an argument (inc or exp). 
+    we define a cariable called sum and set it equal to 0. 
+    */
+    var sum = 0;
+
+    /* for bottom code, example: data.allItems['inc'].forEach... 
+    this adds up all elements in the income array and finds their sum 
+    (simple for loop to calculate sum of items in an array)
+*/
+    data.allItems[type].forEach(function(e) {
+      sum += e.value;
+    });
+
+    // Then just assign the sum to the respective item in the totals (see data structure lines 37-48 below)
+    data.totals[type] = sum;
+  };
+
   // Data structure
   var data = {
     allItems: {
@@ -23,7 +42,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: 0
   };
 
   // budget controller
@@ -70,6 +91,39 @@ var budgetController = (function() {
 
       return newItem;
     },
+
+    // Public Method for calculating budget items:
+
+    calculateBudget: function() {
+      // calculate total income and expenses
+      calculateTotal('inc');
+      calculateTotal('exp');
+
+      // calculate total budget: Income - Expense
+
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // calculate percentage
+      // In case the value of income is 0:
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        /* if value of income is 0, then just assign -1 to percentage. Otherwise JS will assign infinity to it.
+        example: (900 / 0) * 100 = Infinity */
+
+        data.percentage = -1;
+      }
+    },
+    // Public method "getBudget" so GLOBAL controller has access to budget values
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalIncome: data.totals.inc,
+        totalExpenses: data.totals.exp,
+        percent: data.percentage
+      };
+    },
     testDataArray: function() {
       console.log(data);
     }
@@ -96,7 +150,9 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Note: will be either "inc" or "exp"
         description: document.querySelector(DOMstrings.inputDescription).value, // For the description of the budget item
-        value: document.querySelector(DOMstrings.inputValue).value // Value of the budget item
+        value: parseFloat(
+          document.querySelector(DOMstrings.inputValue).value
+        ) /* Value of the budget item. parseFloat to get the floating point value of item. It'll be in form of a string otherwise. Can't do calculations with that! */
       };
     },
     // also return the DOM values so they can be accessible by other modules
@@ -128,19 +184,38 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    // method to clear user input fields after an item has been entered
+
     clearFields: function() {
       var fields, fieldsArray;
 
+      // Select fields you want to clear i.e. in our case, description and value field
       fields = document.querySelectorAll(
         DOMstrings.inputDescription + ', ' + DOMstrings.inputValue
       );
 
-      fieldsArray = Array.prototype.slice.call(fields);
+      /* Important: "querySelectorAll" method gives back a Nodelist. Instead of clearing their values individually,
+      we want to write a loop (DRY). In order to have access to array methods, we can convert the Nodelist into an array . So use ".call()" method to use an Array method (slice) on the fields list
+      for example: fields will look like: 
+      
+      [input.add__description, input.add__value]
 
-      fieldsArray.forEach(function(cur, index, arr) {
+      This is a NodeList. In ES6, this can be converted into an array using : Array.from(fields)
+
+      We can then loop over it
+
+      In older version of JS, it'd be Array.prototype.slice.call(fields)
+
+      Edit to above: Turns out, you can use forEach method on a Nodelist. Nonetheless, I converted it to an array anyway
+      */
+
+      fieldsArray = Array.from(fields);
+
+      fieldsArray.forEach(function(cur) {
         cur.value = '';
       });
 
+      // set focus back to the description box so user can enter another item
       fieldsArray[0].focus();
     }
   };
@@ -178,20 +253,37 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     input = UICtrl.getValues();
 
-    /* 2. Add this input i.e. new item we got from the UI controller (line 112) to the budget controller module by invoking the ".addItem()" method from "budgetController" module. Remember, that method takes "type", "description" and "value" as arguments! (See line 32 of the code above) */
+    var updateBudget = function() {
+      // 1. calculate budget
 
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      budgetCtrl.calculateBudget();
+      // 2. return the budget
 
-    // 3. Add new item to the UI
+      var budget = budgetCtrl.getBudget();
+      // 3. display budget in UI
 
-    UICtrl.addListItem(newItem, input.type);
+      console.log(budget);
+    };
 
-    // 4. Clear fields
+    // Only if the fields (description or value) have not been left empty or item does not have value of 0, then proceed.
 
-    UICtrl.clearFields();
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+      /* 2. Add this input i.e. new item we got from the UI controller (line 112) to the budget controller module by invoking the ".addItem()" method from "budgetController" module. Remember, that method takes "type", "description" and "value" as arguments! (See line 32 of the code above) */
 
-    // 5. calculate budget
-    // 6. Update budget in the UI
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+      // 3. Add new item to the UI
+
+      UICtrl.addListItem(newItem, input.type);
+
+      // 4. Clear fields
+
+      UICtrl.clearFields();
+
+      // 5. update Budget (separated this into another method (see above))
+
+      updateBudget();
+    }
   };
 
   return {
